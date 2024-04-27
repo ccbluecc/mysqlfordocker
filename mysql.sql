07846e1ff6bfe87139756f2eaa88ee453ff0985d
ccbluecc <90882916+ccbluecc@users.noreply.github.com>
2024-04-27 16:30:47 +0700

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,7 @@
+use kradan;
+
+INSERT INTO kradan (title, description, assignees, status, createdOn, updatedOn)
+VALUES ('ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ','あなた、彼、彼女 (私ではありません)','Doing', '2024-04-22 09:10:00', '2024-04-25 00:00:00');
+
+INSERT INTO kradan (title, description, assignees, status, createdOn, updatedOn)
+VALUES ('Infrastructure','Setup containers','ไก่งวง กับ เพนกวิน','Done', '2024-04-22 09:15:00', '2024-04-22 10:00:00');
